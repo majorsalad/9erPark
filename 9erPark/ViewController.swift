f531231dbcc8e50f02999b95628683108e6b6804
jlthompson96 <joeythompson1014@gmail.com>
2021-03-11 15:34:44 -0500

    Testing the comments

--- 9erPark/ViewController.swift
+++ 9erPark/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        // Testing new comment
-        // Dagim your mic is unmuted
+        //Sup Faisal
     }
 
 
