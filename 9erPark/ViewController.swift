c8c32d29f7688f1693fc51b42498a7eeb46f0430
Dagim Worku <dagimsecond@gmaiil.com>
2021-03-16 14:31:35 -0400

    Dagim's second test :)

--- 9erPark/ViewController.swift
+++ 9erPark/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         // Sup Faisal
         // Faisal's branch change
         // Sup Joey and Dagim
-        //Dagim is checking the latest version
+        //Dagim is checking the latest version. 
         // Raplaced line17 code and added images
     }
 
