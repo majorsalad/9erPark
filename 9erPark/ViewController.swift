0c77a56e60dde7fc3e35e52542dc1db64effb965
jlthompson96 <joeythompson1014@gmail.com>
2021-03-18 14:45:49 -0400

    Deleted comments and added one comment to test pull requests.

--- 9erPark/ViewController.swift
+++ 9erPark/ViewController.swift
@@ -11,12 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Deleting previous of lines of code for the Github assignment
-        //Dagim is testing assignment.
-        //Dagim: Test for assignment 
-        //rahul comment
-        //part 2
-        // This is a comment for our Git Immersion hw
+            //Removed comments to test pull requests
     }
 
 
