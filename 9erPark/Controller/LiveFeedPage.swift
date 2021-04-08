8a80a2178fb21391f0be6fd5a16dbcdf5435a788
Faisal Salad <majorsalad@gmail.com>
2021-04-07 21:22:41 -0400

    segue to parking percentages page, w/ custom percent bar

--- 9erPark/Controller/LiveFeedPage.swift
+++ 9erPark/Controller/LiveFeedPage.swift
@@ -9,4 +9,19 @@ import UIKit
 
 class LiveFeedPage : UIViewController {
     
+    
+    var width : CGFloat?
+    var deckName : String?
+    var percent : String?
+    
+    @IBOutlet weak var deck1Label: UILabel!
+    @IBOutlet weak var progressBarLabel: UIView!
+    @IBOutlet weak var percent1Label: UILabel!
+    @IBOutlet weak var progressBarWidth: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        deck1Label.text = deckName!
+        progressBarWidth.constant = width!
+        percent1Label.text = percent!
+    }
 }
