1a3ab3dbbed5a5ef0fb1db052a768aa612267111
Faisal Salad <majorsalad@gmail.com>
2021-04-26 14:20:42 -0400

    renamed image assets and laid groundwork for building image code

--- 9erPark/Controller/OptimalPage.swift
+++ 9erPark/Controller/OptimalPage.swift
@@ -23,6 +23,8 @@ class OptimalPage : UIViewController, ParkingManagerDelegate {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var percentLabel: UILabel!
     @IBOutlet weak var deckProgressBar: NSLayoutConstraint!
+    @IBOutlet weak var buildingImage: UIImageView!
+    
     
     override func viewDidLoad() {
         buildingLabel.text = buildingName
@@ -36,201 +38,259 @@ class OptimalPage : UIViewController, ParkingManagerDelegate {
             case "Athletic Storage":
                 self.firstClosest = "WEST"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Atkins":
                 self.firstClosest = "CD VS"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Auxiliary Services Building":
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Barnard":
                 //East Deck 1 (F/S), 2/3(student), cone deck(F/S)
                //Next closest student is probably union
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Barnhardt Student Activity Center":
                 self.firstClosest = "CD VS"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Baseball Indool Training Facility":
                 self.firstClosest = "WEST"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Belk Gymnasium":
                 self.firstClosest = "WEST"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Belk Hall":
                 self.firstClosest = "UDU"
                 self.secondClosest = "NORTH"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "BES Recycling Facility":
                 self.firstClosest = "NORTH"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Bioinformatics":
                 self.firstClosest = "UDU"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Bissell House":
                 self.firstClosest = "SOUTH"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Burson":
                 self.firstClosest = "UDU"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Cafeteria Activities Building":
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "NORTH"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Cameron Hall":
                 self.firstClosest = "UDU"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Carriage House":
                 self.firstClosest = "SOUTH"
                 self.secondClosest = "WEST"
-                
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Cato College of Education":
                 self.firstClosest = "UDU"
                 self.secondClosest = "WEST"
-                
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Cato Hall":
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Cedar":
                 self.firstClosest = "NORTH"
                 self.secondClosest = "ED2/3"
-                
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Center City Building": //NOT CORRECT!
                 self.firstClosest = "NORTH"
                 self.secondClosest = "ED2/3"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "College of Health & Human Services":
                 self.firstClosest = "UDU"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Colvard":
                 //Cone deck  1/2(F/S), east deck 1(F/S), 2/3, west deck(student)
                 self.firstClosest="ED2/3"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Cone University Center":
                 self.firstClosest = "CD VS"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Denny":
                 //East deck 1(F/S), 2/3(Student), cone deck 1/2(F/S)
                 //next closest is union or west (about .8 miles)
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "WEST"
-                
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Duke Centennial Hall":
                 self.firstClosest = "UDU"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Early College High School":
                 self.firstClosest = "UDU"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "EHS Storage Shed":
                 self.firstClosest = "NORTH"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Elm":
                 self.firstClosest = "UDU"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "EPIC":
                 self.firstClosest = "UDU"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Facilities Annex 3 Greenhouse":
                 self.firstClosest = "NORTH"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Fretwell":
                 //East deck 1(F/S) 2/3, cone deck 1/2
                 //next closest union, west, or south village (.8 miles)
                 self.firstClosest="ED2/3"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Friday":
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Gage Undergraduate Admissions Center":
                 self.firstClosest = "SOUTH"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Grigg":
                 self.firstClosest = "UDU"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Halton Wagner Tennix Complex":
                 self.firstClosest = "WEST"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Harris Alumni Center":
                 self.firstClosest = "SOUTH"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Hawthorne":
                 self.firstClosest = "NORTH"
                 self.secondClosest = "ED2/3"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Hickory":
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "NORTH"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Hunt Hall":
                 self.firstClosest = "SOUTH"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Jerry Richardson Stadium":
                 self.firstClosest = "UDU"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Johnson Band Center":
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Judy W. Rose Football Center":
                 self.firstClosest = "UDU"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Kennedy":
                 self.firstClosest = "CD VS"
                 self.secondClosest = "ED2/3"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "King":
                 self.firstClosest = "CD VS"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Kulwicki Motorsports Laboratory":
                 self.firstClosest = "UDU"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "McEniry":
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Memorial Hall":
                 self.firstClosest = "CD VS"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Miltimore Hall":
                 self.firstClosest = "UDU"
                 self.secondClosest = "NORTH"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Miltimore-Wallis Center":
                 self.firstClosest = "WEST"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Motorsports Research":
                 self.firstClosest = "UDU"
                 self.secondClosest = "NORTH"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Popp Martin Student Union":
                 self.firstClosest = "UDU"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "PORTAL":
                 self.firstClosest = "UDU"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Prospector":
                 self.firstClosest = "CD VS"
                 self.secondClosest = "ED2/3"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Reese":
                 self.firstClosest = "CD VS"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Robinson Hall":
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Sanford Hall":
                 self.firstClosest = "CD VS"
                 self.secondClosest = "SOUTH"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Science Building":
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Scott Hall":
                 self.firstClosest = "SOUTH"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Smith":
                 self.firstClosest = "CD VS"
                 self.secondClosest = "WEST"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Wells Fargo Field House":
                 self.firstClosest = "WEST"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Winningham":
                 self.firstClosest = "ED2/3"
                 self.secondClosest = "CD VS"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Witherspoon":
                 self.firstClosest = "NORTH"
                 self.secondClosest = "UDU"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             case "Woodward Hall":
                 //West deck, union(Student), cone 2, 1, south village (student)
                 self.firstClosest="WEST"
                 self.secondClosest = "SOUTH"
+                self.buildingImage.image = UIImage(named: "woodward-hall")
             default:
                 print(parkingDict["CD VS"]!)
                 }
